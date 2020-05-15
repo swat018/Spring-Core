18f552b561a5a3d16d4c6945df299a00e86f515c
jinwoo park <swat018@gmail.com>
2020-05-15 23:30:29 +0900

    IoC - messageSource

--- src/main/java/com/swat018/springcore/AppRunner.java
+++ src/main/java/com/swat018/springcore/AppRunner.java
@@ -1,31 +1,27 @@
 package com.swat018.springcore;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
-
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext ctx;
+    MessageSource messageSource;
 
-    @Autowired
-    BookReposiitory bookReposiitory;
-
-    @Value("${app.name}")
-    String appName;
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = ctx.getEnvironment();
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(appName);
+
+        while(true) {
+            System.out.println(messageSource.getMessage("greeting", new String[]{"jinwoo"}, Locale.KOREA));
+            System.out.println(messageSource.getMessage("greeting", new String[]{"jinwoo"}, Locale.getDefault()));
+            Thread.sleep(1000);
+        }
+
     }
 }
--- src/main/java/com/swat018/springcore/SpringCoreApplication.java
+++ src/main/java/com/swat018/springcore/SpringCoreApplication.java
@@ -2,13 +2,25 @@ package com.swat018.springcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
-@PropertySource("classpath:/app.properties")
 public class SpringCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCoreApplication.class, args);
+
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+        return messageSource;
     }
 
 }
