b621522854b28f9da2d30d6486052bfa71412670
jinwoo park <swat018@gmail.com>
2020-05-16 12:59:47 +0900

    IOC - ResourceLoader

--- src/main/java/com/swat018/springcore/AppRunner.java
+++ src/main/java/com/swat018/springcore/AppRunner.java
@@ -3,18 +3,25 @@ package com.swat018.springcore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.boot.ResourceBanner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher publisherEvent;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        publisherEvent.publishEvent(new MyEvent(this,100));
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
--- src/main/java/com/swat018/springcore/SpringCoreApplication.java
+++ src/main/java/com/swat018/springcore/SpringCoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class SpringCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCoreApplication.class, args);
