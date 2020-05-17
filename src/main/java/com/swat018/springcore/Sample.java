7bf96574d0a5247584ffd82d3d76d8d009b570f3
jinwoo park <swat018@gmail.com>
2020-05-17 16:54:41 +0900

    SpEL (스프링 Expression Language)

---
+++ src/main/java/com/swat018/springcore/Sample.java
@@ -0,0 +1,16 @@
+package com.swat018.springcore;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+    private int data = 200;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
